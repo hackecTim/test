4aadd1686427eecd19fe1fcc36bf2a0e8c10ecb5
aj91854 <155680510+aj91854@users.noreply.github.com>
2025-12-12 21:11:25 +0100

    Fix case sensitivity in SQL table name

--- Scripts/add_review.php
+++ Scripts/add_review.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "config.php";
+include "Config.php";
 
 if (!isset($_SESSION['userID'])) {
     header("Location: ../Sites/login.php?error=login_required");
@@ -17,7 +17,7 @@ if ($rating < 1 || $rating > 5) {
     exit();
 }
 
-$sql = "INSERT INTO review (userID, placeID, rating, comment) VALUES (?,?,?,?)";
+$sql = "INSERT INTO Review (userID, placeID, rating, comment) VALUES (?,?,?,?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("iiis", $userID, $placeID, $rating, $comment);
 $stmt->execute();
@@ -25,3 +25,4 @@ $stmt->execute();
 header("Location: ../pages-layout/page_layout.php?id=$placeID&review=success");
 exit();
 ?>
+
