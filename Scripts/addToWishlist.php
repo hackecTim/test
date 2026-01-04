d2c4df1c2e818617f158ae7da93986f23e65e97d
aj91854 <155680510+aj91854@users.noreply.github.com>
2026-01-04 02:12:50 +0100

    Fix case sensitivity

--- Scripts/addToWishlist.php
+++ Scripts/addToWishlist.php
@@ -14,7 +14,7 @@ if (!isset($_POST['placeID'])) {
 $userID  = (int) $_SESSION['userID'];
 $placeID = (int) $_POST['placeID'];
 
-$checkSql = "SELECT 1 FROM wishlist WHERE userID = ? AND placeID = ?";
+$checkSql = "SELECT 1 FROM Wishlist WHERE userID = ? AND placeID = ?";
 $checkStmt = $conn->prepare($checkSql);
 $checkStmt->bind_param("ii", $userID, $placeID);
 $checkStmt->execute();
@@ -25,7 +25,7 @@ if ($checkStmt->num_rows > 0) {
     exit;
 }
 
-$insertSql = "INSERT INTO wishlist (userID, placeID) VALUES (?, ?)";
+$insertSql = "INSERT INTO Wishlist (userID, placeID) VALUES (?, ?)";
 $insertStmt = $conn->prepare($insertSql);
 $insertStmt->bind_param("ii", $userID, $placeID);
 
